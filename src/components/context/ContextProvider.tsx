c398f6cdf687fcd2cda47a7d40d17aeab92a3bbe
Przemeks200iq <vertezelopl@gmail.com>
2022-07-02 00:03:33 +0200

    Edit bug in ContextProvider

--- src/components/context/ContextProvider.tsx
+++ src/components/context/ContextProvider.tsx
@@ -1,14 +1,22 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useMemo, useState } from 'react';
+
+interface StateContextProviderProps {
+  foo: number;
+  setFoo: React.Dispatch<React.SetStateAction<number>>;
+}
 
 // @ts-ignore
-const StateContext = createContext();
+const StateContext = createContext<StateContextProviderProps>();
 
 export const ContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [foo, setFoo] = useState<number>();
+  const [foo, setFoo] = useState<number>(2);
+
+  const memorizedValue = useMemo(() => ({ foo, setFoo }), [foo, setFoo]);
+
   return (
-    <StateContext.Provider value={{ foo, setFoo }}>
+    <StateContext.Provider value={memorizedValue}>
       {children}
     </StateContext.Provider>
   );
